cf39761d1e26e42c9aeab18d5b456be1ea2474de
fitzpatricksrus <junk@fitzpatircksr.us>
2019-07-13 16:41:01 -0700

    Fixed an OMG! bug in snoop on/off methods and made things more usable by guessing package names and adding ability to ignore specific events.

--- src/main/java/com/example/examplemod/utilities/eventsnoopers/EventSnooper.java
+++ src/main/java/com/example/examplemod/utilities/eventsnoopers/EventSnooper.java
@@ -3,47 +3,50 @@ package com.example.examplemod.utilities.eventsnoopers;
 import com.example.examplemod.utilities.Logging;
 import com.example.examplemod.utilities.commands.Command;
 import com.example.examplemod.utilities.commands.GenericCommand;
-import com.example.examplemod.utilities.hackfmlevents.HackFMLEventListener;
 import net.minecraft.command.ICommandSender;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.*;
 
-public class EventSnooper implements HackFMLEventListener {
+public class EventSnooper {
 	private static final String COMMAND_NAME = "snoop";
-	private static final String COMMAND_USAGE = "/snoop <event> [ ON | OFF ]";
+	private static final String COMMAND_USAGE = "/snoop on | off | <event> [ ON | OFF | IGNORE ]";
 	private static final String[] COMMAND_ALIASES = {};
 	public static EventSnooper INSTANCE;
 	private boolean snooping = false;
 	private Set<Class<?>> snoopingTypes = new HashSet<>();
+	private static final String[] packages = {
+			"net.minecraftforge.event.",
+			"net.minecraftforge.event.brewing.",
+			"net.minecraftforge.event.enchanting.",
+			"net.minecraftforge.event.entity.",
+			"net.minecraftforge.event.entity.item.",
+			"net.minecraftforge.event.entity.living.",
+			"net.minecraftforge.event.entity.minecart.",
+			"net.minecraftforge.event.entity.player.",
+			"net.minecraftforge.event.furnace.",
+			"net.minecraftforge.event.terraingen.",
+			"net.minecraftforge.event.village.",
+			"net.minecraftforge.event.world.",
+			"net.minecraftforge.client.event.",
+			"net.minecraftforge.client.event.sound"
+	};
+
+	//---------------------------------------------------------------------------
+	//---------------------------------------------------------------------------
+	private Set<Class<?>> snoopingIgnoreTypes = new HashSet<>();
+
+	//---------------------------------------------------------------------------
+	//---------------------------------------------------------------------------
 
 	public EventSnooper() {
-		subscribeToFMLEvents();
 		GenericCommand.create(COMMAND_NAME, COMMAND_USAGE, COMMAND_ALIASES).addTarget(this);
 	}
 
 	public static void init() {
 		INSTANCE = new EventSnooper();
-	}
-
-	@Override
-	public void handleFMLEvent(FMLPreInitializationEvent event) {
-	}
-
-	@SubscribeEvent
-	public void snoopEvent(Event e) {
-		if (snooping) {
-			Class<?> eventClass = e.getClass();
-			for (Class<?> clazz : snoopingTypes) {
-				if (clazz.isAssignableFrom(eventClass)) {
-					Logging.logInfo(">> event: " + e.getClass().getName());
-					Logging.logInfo(">>      : " + e.toString());
-				}
-			}
-		}
 	}
 
 	@Command
@@ -56,8 +59,13 @@ public class EventSnooper implements HackFMLEventListener {
 		doDisable(sender);
 	}
 
+	public void setBreakpointHere(Event e) {
+		Logging.logInfo(">> event: " + e.getClass().getName());
+	}
+
 	@Command
 	public void doEnable(ICommandSender sender) {
+		snooping = true;
 		if (snoopingTypes.size() > 0) {
 			MinecraftForge.EVENT_BUS.register(this);
 		}
@@ -65,7 +73,23 @@ public class EventSnooper implements HackFMLEventListener {
 
 	@Command
 	public void doDisable(ICommandSender sender) {
+		snooping = false;
 		MinecraftForge.EVENT_BUS.unregister(this);
+	}
+
+	@Command
+	public void doOn(ICommandSender sender, String event) {
+		doIt(sender, event, "on");
+	}
+
+	@Command
+	public void doOff(ICommandSender sender, String event) {
+		doIt(sender, event, "off");
+	}
+
+	@Command
+	public void doIgnore(ICommandSender sender, String event) {
+		doIt(sender, event, "ignore");
 	}
 
 	@Command
@@ -75,37 +99,98 @@ public class EventSnooper implements HackFMLEventListener {
 
 	@Command
 	public void doIt(ICommandSender sender, String eventClassName, String onOff) {
-		try {
-			Class<?> eventClass = Class.forName(eventClassName);
+		Class<?> eventClass = findEventClass(eventClassName);
+		if (eventClass != null) {
 			onOff = onOff.toLowerCase();
 			if ("on".equals(onOff)) {
+				snoopingIgnoreTypes.remove(eventClass);
 				snoopingTypes.add(eventClass);
 				if (snooping && (snoopingTypes.size() == 1)) {
 					MinecraftForge.EVENT_BUS.register(this);
 				}
+				GenericCommand.sendMsg(sender, "Snooping enabled for " + eventClass.getName());
 			} else if ("off".equals(onOff)) {
 				snoopingTypes.remove(eventClass);
 				if (snoopingTypes.size() == 0) {
 					MinecraftForge.EVENT_BUS.unregister(this);
 				}
+				GenericCommand.sendMsg(sender, "Snooping disabled for " + eventClass.getName());
+			} else if ("ignore".equals(onOff)) {
+				if (!snoopingTypes.remove(eventClass)) {
+					snoopingIgnoreTypes.add(eventClass);
+					GenericCommand.sendMsg(sender, "Snooping ignores " + eventClass.getName());
+				} else {
+					if (snoopingTypes.size() == 0) {
+						MinecraftForge.EVENT_BUS.unregister(this);
+					}
+					GenericCommand.sendMsg(sender, "Snooping disabled for " + eventClass.getName());
+				}
 			} else {
 				GenericCommand.sendMsg(sender, COMMAND_USAGE);
 			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		} else {
 			GenericCommand.sendMsg(sender, "Could not find class: " + eventClassName);
 		}
 	}
 
 	@Command
 	public void doList(ICommandSender sender) {
-		List<String> typeNames = new ArrayList<>(snoopingTypes.size());
-		for (Class<?> clazz : snoopingTypes) {
+		GenericCommand.sendMsg(sender, "Snooping is " + ((snooping) ? "on" : "off"));
+		GenericCommand.sendMsg(sender, "Snooping for:");
+		for (String name : getNameList(snoopingTypes)) {
+			GenericCommand.sendMsg(sender, "  " + name);
+		}
+		if (snoopingIgnoreTypes.size() > 0) {
+			GenericCommand.sendMsg(sender, "Snooping ignores:");
+			for (String name : getNameList(snoopingIgnoreTypes)) {
+				GenericCommand.sendMsg(sender, "  " + name);
+			}
+		}
+	}
+
+	private List<String> getNameList(Set<Class<?>> classes) {
+		List<String> typeNames = new ArrayList<>(classes.size());
+		for (Class<?> clazz : classes) {
 			typeNames.add(clazz.getName());
 		}
 		Collections.sort(typeNames);
-		for (String name : typeNames) {
-			GenericCommand.sendMsg(sender, name);
+		return typeNames;
+	}
+
+	@SubscribeEvent
+	public void snoopEvent(Event e) {
+		if (snooping) {
+			Class<?> eventClass = e.getClass();
+			if (!snoopingIgnoreTypes.contains(eventClass)) {
+				for (Class<?> clazz : snoopingTypes) {
+					if (clazz.isAssignableFrom(eventClass)) {
+						setBreakpointHere(e);
+					}
+				}
+			}
+		}
+	}
+
+	private Class<?> findEventClass(String name) {
+		Class<?> result = findClass(name);
+		if (result != null) {
+			return result;
+		} else {
+			for (String packagePrefix : packages) {
+				result = findClass(packagePrefix + name);
+				if (result != null) {
+					return result;
+				}
+			}
+		}
+		return null;
+	}
+
+	private Class<?> findClass(String name) {
+		try {
+			return Class.forName(name);
+		} catch (ClassNotFoundException e) {
+			return null;
 		}
 	}
 }
